dc6b23c348f643defb5107a0a936d17ca5f3d9bd
ew-prog <ewanyama@live.co.uk>
2025-10-19 06:08:08 -0700

    Update Lead.js

--- models/Lead.js
+++ models/Lead.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const leadSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String,
-  insuranceCompany: String,
-  product: String,
-}, { timestamps: true })
+const leadSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    company: { type: String, required: true },
+    product: { type: String },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("Lead", leadSchema)
+export default mongoose.model("Lead", leadSchema);
