978b67e3aec790cc8cff8896b5063c30f7a2c625
ew-prog <ewanyama@live.co.uk>
2025-10-19 09:13:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import LeadRoutes from "./routes/leadRoutes.js"; // router
+import LeadRoutes from "./routes/LeadRoutes.js"; // router
 
 dotenv.config();
 
