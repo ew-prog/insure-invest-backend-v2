79329df9ff2e2a434c28290e2ab7c5047a2d1305
ew-prog <ewanyama@live.co.uk>
2025-10-19 09:04:27 -0700

    Update LeadRoutes.js

--- routes/LeadRoutes.js
+++ routes/LeadRoutes.js
@@ -3,14 +3,6 @@ import { createLead, getLeads } from "../controllers/leadsController.js";
 
 const router = express.Router();
 
-// Enable CORS for this router (optional, already enabled globally)
-router.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 // Routes
 router.post("/", createLead);
 router.get("/", getLeads);
