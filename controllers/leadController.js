fd2f14aa3507cc954f158eead7dc43da1c59f7b1
ew-prog <ewanyama@live.co.uk>
2025-10-19 04:51:39 -0700

    Create leadController.js

---
+++ controllers/leadController.js
@@ -0,0 +1,20 @@
+import Lead from '../models/Lead.js';
+
+export const getLeads = async (req, res) => {
+  try {
+    const leads = await Lead.find();
+    res.json(leads);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const createLead = async (req, res) => {
+  try {
+    const lead = new Lead(req.body);
+    await lead.save();
+    res.status(201).json(lead);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
